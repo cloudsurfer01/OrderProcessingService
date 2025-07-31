ec376dc1d135ba1af603d2e6af908c7416e272ff
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-01 00:18:45 +0200

    Add ValueObject: Address

---
+++ src/Domain/ValueObjects/Address.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.ValueObjects;
+
+public class Address
+{
+    public string Value { get; }
+
+    public Address(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException("Address cannot be empty.");
+        Value = value;
+    }
+    
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Address other) return false;
+        return Value == other.Value;
+    }
+
+    public override int GetHashCode() => Value.GetHashCode();
+}
+
