19dcf498efbcaa89f0cb5c369f127d99bd73a5ca
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-01 03:41:43 +0200

    Add unit test for ValueObject: Email

---
+++ tests/unitTests/Domain.Tests/ValueObjects/EmailTests.cs
@@ -0,0 +1,26 @@
+﻿using Domain.ValueObjects;
+
+namespace Domain.Tests.ValueObjects;
+
+public class EmailTests
+{
+    [Fact]
+    public void Constructor_Should_Create_Email_When_Valid()
+    {
+        var email = new Email("ikhan@paessler.com");
+        Assert.Equal("ikhan@paessler.com", email.Value);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("invalid-email")]
+    [InlineData("ikhan@paessler")]
+    [InlineData("ikhan@paessler,com")]
+    [InlineData("ikhan@paessler..com")]
+    public void Constructor_Should_Throw_Exception_When_Invalid(string param)
+    {
+        Assert.Throws<ArgumentException>(() => new Email(param));
+    }
+    
+}
+
