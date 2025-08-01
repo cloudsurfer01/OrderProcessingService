bda77a3150e1e84342449f69fe1188345dd99843
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-02 00:52:01 +0200

    feat(application): add CreateOrderCommand for order creation via MediatR

---
+++ src/Application/Orders/Commands/CreateOrderCommand.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTOs;
+using MediatR;
+
+namespace Application.Orders.Commands;
+
+public class CreateOrderCommand(CreateOrderRequest request) : IRequest<OrderResponse>
+{
+    public CreateOrderRequest Request { get; } = request;
+}
