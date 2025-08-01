d847df0677083ef3405106dce163f95e39d11491
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-01 23:03:19 +0200

    feat(application): add CreateOrderRequest DTO with embedded ProductItem

---
+++ src/Application/DTOs/CreateOrderRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace Application.DTOs;
+
+public class CreateOrderRequest
+{
+    public List<ProductItem> Products { get; set; } = new ();
+    public string InvoiceAddress { get; set; } = string.Empty;
+    public string DeliveryAddress { get; set; } = string.Empty;
+    public string InvoiceCreditCardNumber { get; set; } = string.Empty;
+}
+
+public class ProductItem
+{
+    public string ProductId { get; set; } = string.Empty;
+    public string ProductName { get; set; } = string.Empty;
+    public int ProductAmount { get; set; }
+    public decimal ProductPrice { get; set; } = 0.0m;
+}
+
+
