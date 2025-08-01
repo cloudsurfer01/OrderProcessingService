5a44bde856045d73b77185100af232e1caf493ce
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-02 01:20:28 +0200

    feat(domain): add IOrderRepository abstraction for order persistence

---
+++ src/Domain/Abstractions/Repositories/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Domain.Abstractions.Repositories;
+
+public interface IOrderRepository
+{
+    Task AddAsync(Order order, CancellationToken cancellationToken = default);
+    Task<Order?> GerByOrderNumberAsync(string orderNumber, CancellationToken cancellationToken = default);
+}
+
