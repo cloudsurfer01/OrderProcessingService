79b39b5d08df3ebe3f4318c832269693a2bad056
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-01 16:28:46 +0200

    Add unit tests for ValueObject: Address

---
+++ tests/unitTests/Domain.Tests/ValueObjects/AddressTests.cs
@@ -0,0 +1,23 @@
+﻿using Domain.ValueObjects;
+
+namespace Domain.Tests.ValueObjects;
+
+public class AddressTests
+{
+    [Fact]
+    public void Constructor_Should_Create_Address_When_Valid()
+    {
+        var address = new Address("123 Sample Street, 90402 Berlin");
+
+        Assert.Equal("123 Sample Street, 90402 Berlin", address.Value);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void Constructor_Should_Throw_When_Invalid(string input)
+    {
+        Assert.Throws<ArgumentException>(() => new Address(input));
+    }
+}
+
