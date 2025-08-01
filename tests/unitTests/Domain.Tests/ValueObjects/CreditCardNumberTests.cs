9097df391fa3ca28af82201bd73c63e723102bbe
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-01 16:29:12 +0200

    Add unit tests for ValueObject: CreditCardNumber

---
+++ tests/unitTests/Domain.Tests/ValueObjects/CreditCardNumberTests.cs
@@ -0,0 +1,25 @@
+﻿using Domain.ValueObjects;
+using Xunit;
+
+namespace Domain.Tests.ValueObjects;
+
+public class CreditCardNumberTests
+{
+    [Fact]
+    public void Constructor_Should_Create_CreditCardNumber_When_Valid()
+    {
+        var card = new CreditCardNumber("1234-5678-9012-3456");
+
+        Assert.Equal("1234567890123456", card.Value);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    [InlineData("1234")]
+    [InlineData("123456789012345678901")]
+    public void Constructor_Should_Throw_When_Invalid(string input)
+    {
+        Assert.Throws<ArgumentException>(() => new CreditCardNumber(input));
+    }
+}
