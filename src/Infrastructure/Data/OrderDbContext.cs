ddf1e21d027c63e3b841bbcf0e6238be7cacc520
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-02 14:59:00 +0200

    feat(infrastructure): add OrderDbContext with aggregate root configuration

---
+++ src/Infrastructure/Data/OrderDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class OrderDbContext(DbContextOptions<OrderDbContext> options) : DbContext(options)
+{
+    public DbSet<Order> Orders => Set<Order>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(OrderDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
