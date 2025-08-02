3063ce8a0dafb40a45170be7f99366581711aa3c
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-02 12:07:59 +0200

    feat(application): add GetOrderByOrderNumberQuery for retrieving order by ID

---
+++ src/Application/Orders/Queries/GetOrderByOrderNumberQuery.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTOs;
+using MediatR;
+
+namespace Application.Orders.Queries;
+
+public class GetOrderByOrderNumberQuery(string orderNumber) : IRequest<OrderResponse>
+{
+    public string OrderNumber { get; } = orderNumber;
+}
