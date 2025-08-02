4905b7f5d1429e07b1c000d0a9e2e1be2ad70f44
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-03 00:06:00 +0200

    chore(infrastructure): add design-time OrderDbContextFactory for EF Core CLI support

---
+++ src/Infrastructure/Data/OrderDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Data;
+
+public class OrderDbContextFactory : IDesignTimeDbContextFactory<OrderDbContext>
+{
+    public OrderDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Development.json")
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<OrderDbContext>();
+        optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+
+        return new OrderDbContext(optionsBuilder.Options);
+    }
+}
