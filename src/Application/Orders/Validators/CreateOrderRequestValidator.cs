f0c4e1bbd0dd00db31e59b126b383e5b71909dfe
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-02 11:20:26 +0200

    feat(application): add CreateOrderRequestValidator and ProductItemValidator using FluentValidation

---
+++ src/Application/Orders/Validators/CreateOrderRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using Application.DTOs;
+using FluentValidation;
+
+namespace Application.Orders.Validators;
+
+public class CreateOrderRequestValidator : AbstractValidator<CreateOrderRequest>
+{
+    public CreateOrderRequestValidator()
+    {
+        RuleFor(x => x.InvoiceAddress)
+            .NotEmpty()
+            .WithMessage("Invoice address is required.");
+
+        RuleFor(x => x.InvoiceEmailAddress)
+            .NotEmpty().WithMessage("Invoice Email address is required.")
+            .EmailAddress().WithMessage("Invoice Email address format in invalid");
+
+        RuleFor(x => x.InvoiceCreditCardNumber)
+            .NotEmpty().WithMessage("Invoice Credit Card Number is required.")
+            .CreditCard().WithMessage("Invoice Credit Card Number format is invalid");
+
+        RuleForEach(x => x.Products).SetValidator(new ProductItemValidator());
+    }
+}
---
+++ src/Application/Orders/Validators/ProductItemValidator.cs
@@ -0,0 +1,16 @@
+﻿using Application.DTOs;
+using FluentValidation;
+
+namespace Application.Orders.Validators
+{
+    public class ProductItemValidator : AbstractValidator<ProductItem>
+    {
+        public ProductItemValidator()
+        {
+            RuleFor(x => x.ProductId).NotEmpty();
+            RuleFor(x => x.ProductName).NotEmpty();
+            RuleFor(x => x.ProductAmount).GreaterThan(0);
+            RuleFor(x => x.ProductPrice).GreaterThan(0);
+        }
+    }
+}
