eef7063fc699a222ef343783531f0fdf863f93ef
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-02 12:44:59 +0200

    feat(refactor): update method name

--- src/Domain/Abstractions/Repositories/IOrderRepository.cs
+++ src/Domain/Abstractions/Repositories/IOrderRepository.cs
@@ -5,6 +5,6 @@ namespace Domain.Abstractions.Repositories;
 public interface IOrderRepository
 {
     Task AddAsync(Order order, CancellationToken cancellationToken = default);
-    Task<Order?> GerByOrderNumberAsync(string orderNumber, CancellationToken cancellationToken = default);
+    Task<Order?> GetByOrderNumberAsync(string orderNumber, CancellationToken cancellationToken = default);
 }
 
