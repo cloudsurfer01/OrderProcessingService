0bbff0e33fee3d7d67fbc36d9e674ddee5bda448
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-02 12:58:33 +0200

    feat(chore): remove extra property from CreateOrderReqeust

--- src/Application/DTOs/CreateOrderRequest.cs
+++ src/Application/DTOs/CreateOrderRequest.cs
@@ -4,7 +4,6 @@ public class CreateOrderRequest
 {
     public List<ProductItem> Products { get; set; } = new ();
     public string InvoiceAddress { get; set; } = string.Empty;
-    public string DeliveryAddress { get; set; } = string.Empty;
     public string InvoiceCreditCardNumber { get; set; } = string.Empty;
     public string InvoiceEmailAddress { get; set; } = string.Empty;
 }
