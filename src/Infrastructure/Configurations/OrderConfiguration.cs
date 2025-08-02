abcd22c29b5822250bb598530f43262aaf7b8db7
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-02 15:48:23 +0200

    feat(infrastructure): configure Order entity with owned Product value objects.

---
+++ src/Infrastructure/Configurations/OrderConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> orderBuilder)
+    {
+        orderBuilder.HasKey(o => o.OrderNumber);
+
+        orderBuilder.Property(o => o.InvoiceAddress).IsRequired();
+        orderBuilder.Property(o => o.InvoiceEmailAddress).IsRequired();
+        orderBuilder.Property(o => o.CreditCardNumber).IsRequired();
+        orderBuilder.Property(o => o.CreatedAt).IsRequired();
+
+        orderBuilder.OwnsMany(o => o.Products, productBuilder =>
+        {
+            productBuilder.WithOwner().HasForeignKey("OrderNumber");
+            productBuilder.Property(p => p.ProductId).IsRequired();
+            productBuilder.Property(p => p.ProductName).IsRequired().HasMaxLength(100);
+            productBuilder.Property(p => p.ProductPrice).IsRequired().HasColumnType("decimal(18,2)");
+            productBuilder.Property(p => p.ProductAmount).IsRequired();
+            productBuilder.ToTable("OrderProducts");
+        });
+    }
+}
