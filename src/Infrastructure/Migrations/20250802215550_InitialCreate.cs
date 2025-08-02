ec16bcf542f9091d61cd64165af234368cd14152
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-03 00:16:37 +0200

    feat(infrastructure): generate initial migration for Order and OrderProducts schema

---
+++ src/Infrastructure/Migrations/20250802215550_InitialCreate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderNumber = table.Column<Guid>(type: "uuid", nullable: false),
+                    InvoiceAddress = table.Column<string>(type: "text", nullable: true),
+                    InvoiceEmailAddress = table.Column<string>(type: "text", nullable: true),
+                    CreditCardNumber = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderNumber);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderProducts",
+                columns: table => new
+                {
+                    ProductId = table.Column<string>(type: "text", nullable: false),
+                    OrderNumber = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ProductAmount = table.Column<int>(type: "integer", nullable: false),
+                    ProductPrice = table.Column<decimal>(type: "numeric(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderProducts", x => new { x.OrderNumber, x.ProductId });
+                    table.ForeignKey(
+                        name: "FK_OrderProducts_Orders_OrderNumber",
+                        column: x => x.OrderNumber,
+                        principalTable: "Orders",
+                        principalColumn: "OrderNumber",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderProducts");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
