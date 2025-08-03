0190616f309b3485a2a5fbf8ffc30f9fa31c56da
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-03 20:37:06 +0200

    fix(Api): add route to controller for swagger discovery for action methods.

--- src/Api/Controllers/OrderController.cs
+++ src/Api/Controllers/OrderController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
 
+[Route("[controller]")]
+[ApiController]
 public class OrderController(IMediator mediator) : Controller
 {
     [HttpPost]
