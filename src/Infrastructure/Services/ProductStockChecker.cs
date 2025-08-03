35058c71dd8ad260fbe1d70b8bb5120f01ce243b
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-03 16:10:55 +0200

    feat(infrasturcture): add ProductStockChecker to check if product exist in stock.

---
+++ src/Infrastructure/Services/ProductStockChecker.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+using Domain.Abstractions.Repositories;
+
+namespace Infrastructure.Services;
+public class ProductStockChecker : IProductStockChecker
+{
+    public async Task<bool> IsProductInStockAsync(string productId, int quantity, CancellationToken cancellationToken = default)
+    {
+        return true;
+    }
+}
