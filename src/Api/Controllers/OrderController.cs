2fed03351ea897892ab0845ba273b8fce945294c
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-03 14:14:49 +0200

    feat(api): add OrdersController with POST and GET endpoints

---
+++ src/Api/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTOs;
+using Application.Orders.Commands;
+using Application.Orders.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+public class OrderController(IMediator mediator) : Controller
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateOrder([FromBody] CreateOrderRequest request)
+    {
+        var command = new CreateOrderCommand(request);
+        var orderNumber = await mediator.Send(command);
+        return CreatedAtAction(nameof(GetOrderByNumber), new { orderNumber }, new { orderNumber });
+    }
+
+    [HttpGet("{orderNumber}")]
+    public async Task<IActionResult> GetOrderByNumber(string orderNumber)
+    {
+        var command = new GetOrderByOrderNumberQuery(orderNumber);
+        var response = await mediator.Send(command);
+        return Ok(response);
+    }
+}
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,6 +1,8 @@
 using Application.Orders.Commands;
 using Application.Orders.Validators;
 using FluentValidation;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +14,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssembly(typeof(CreateOrderCommand).Assembly));
 builder.Services.AddValidatorsFromAssemblyContaining<CreateOrderRequestValidator>();
+builder.Services.AddDbContext<OrderDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
