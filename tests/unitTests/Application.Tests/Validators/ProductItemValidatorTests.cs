69dc55950a5ed85b473f3ec8452d673eb6b6cf3b
Muhammad Imran Khan <imran.khan@ebitlogix.com>
2025-08-04 06:03:17 +0200

    test(application, validation): add tests for ProductItemValidator

---
+++ tests/unitTests/Application.Tests/Validators/ProductItemValidatorTests.cs
@@ -0,0 +1,53 @@
+﻿using Application.DTOs;
+using Application.Orders.Validators;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace Application.Tests.Validators;
+
+public class ProductItemValidatorTests
+{
+    private readonly ProductItemValidator _validator = new();
+
+    [Fact]
+    public void Should_Have_Error_When_ProductItem_Is_Invalid()
+    {
+        // Arrange
+        var item = new ProductItem
+        {
+            ProductId = "",
+            ProductName = "",
+            ProductAmount = 0,
+            ProductPrice = 0
+        };
+
+        // Act
+        var result = _validator.TestValidate(item);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(i => i.ProductId);
+        result.ShouldHaveValidationErrorFor(i => i.ProductName);
+        result.ShouldHaveValidationErrorFor(i => i.ProductAmount);
+        result.ShouldHaveValidationErrorFor(i => i.ProductPrice);
+    }
+
+    [Fact]
+    public void Should_Not_Have_Error_When_ProductItem_Is_Valid()
+    {
+        // Arrange
+        var item = new ProductItem
+        {
+            ProductId = "123",
+            ProductName = "Phone",
+            ProductAmount = 2,
+            ProductPrice = 499.99m
+        };
+
+        // Act
+        var result = _validator.TestValidate(item);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+}
